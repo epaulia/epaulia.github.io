5e39924ce02281fb30e8a77575fb10b3fc3d16af
pvulya1 <65059433+pvulya1@users.noreply.github.com>
2020-05-24 17:11:48 -0600

    Add files via upload
    dist change

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/red_arrow.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right_arrow.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/red_arrow.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right_arrow.svg"></button>',
         responsive: [
             {
                 breakpoint: 768,
